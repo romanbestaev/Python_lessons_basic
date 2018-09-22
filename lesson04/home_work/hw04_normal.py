2d6431daf2fa41f86ce5ac1d3b1496563625428f
romanbestaev <romanbestaev@yandex.ru>
2018-09-22 14:58:08 +0300

    ДЗ 4normal

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,5 @@
+﻿import re
+import random
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -19,7 +21,15 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
-
+list_lower_1 = re.findall('[a-z]+',line)
+list_lower_2 = ['']
+for x in line:
+    if x.isupper():
+        if len(list_lower_2[-1]):
+            list_lower_2.append('')
+    else:
+        list_lower_2[-1]+=x
+print('Task 1','\nmethod_re = \n',list_lower_1,'\nmethod_x = \n',list_lower_2,sep='')
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -44,6 +54,20 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+list_upper_1 = re.findall('[a-z]{2}([A-Z]+)[A-Z]{2}',line_2)
+list_upper_2 = []
+for i in range(2,len(line_2)-2):
+    if line_2[i-2:i].islower() and line_2[i:i+3].isupper():
+        list_upper_2.append('')
+        j = 0
+        while i+j < len(line_2):
+            if line_2[i+j+2].isupper():
+                list_upper_2[-1] += line_2[i+j]
+                j += 1
+            else:
+                break
+        i += j+2
+print('\n\nTask 2','\nmethod_re = \n',list_upper_1,'\nmethod_x = \n',list_upper_2,sep='')
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -51,3 +75,19 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+fid = open('random_digits.txt','w')
+for i in range(0,2500):
+    fid.write(str(random.randint(0,9)))
+fid.close()
+line_3 = open('random_digits.txt','r').read()
+max_line = str(line_3[0])
+x_line = ''
+for i in range(1,len(line_3)):
+    if line_3[i] == line_3[i-1]:
+        x_line += str(line_3[i])
+        if len(x_line) > len(max_line):
+            max_line = x_line
+    else:
+        x_line = str(line_3[i])
+print('\n\nTask 3','\nmax_line = ',max_line,sep='')
+
